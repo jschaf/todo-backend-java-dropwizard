ce75039beb1053e817d5cd20e34997e67e5bda4e
Joe Schafer <joe@jschaf.com>
2016-01-11 22:26:09 -0500

    feat: add post

--- src/main/java/com/example/todo/resources/TodoResource.java
+++ src/main/java/com/example/todo/resources/TodoResource.java
@@ -3,15 +3,11 @@ package com.example.todo.resources;
 import com.codahale.metrics.annotation.Timed;
 import com.example.todo.api.Entry;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 
 @Path("/")
-@Produces(MediaType.APPLICATION_JSON)
 public class TodoResource {
 
     public TodoResource() {
@@ -19,7 +15,15 @@ public class TodoResource {
 
     @GET
     @Timed
+    @Produces(MediaType.APPLICATION_JSON)
     public Entry getEntry() {
         return new Entry(100, "MY AWESOME TITLE", true, 99);
+    }
+
+    @POST
+    @Timed
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putEntry(Entry entry) {
+
     }
 }
