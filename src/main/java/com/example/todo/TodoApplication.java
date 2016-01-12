751a605c3820879aa55e980fc9eba7cfbf9822bb
Joe Schafer <joe@jschaf.com>
2016-01-11 22:16:48 -0500

    fix: add cross origin support

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -4,6 +4,11 @@ import com.example.todo.resources.TodoResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TodoApplication extends Application<TodoConfiguration> {
 
@@ -14,9 +19,17 @@ public class TodoApplication extends Application<TodoConfiguration> {
     @Override
     public void run(TodoConfiguration configuration, Environment environment) throws Exception {
         final TodoResource todoResource = new TodoResource();
-
         environment.jersey().register(todoResource);
 
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+            // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 
     @Override
